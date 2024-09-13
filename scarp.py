b194074fb48ece1c6ff74572e465e94e654ced2f
valentina <valentinasalmon3@gmail.com>
2024-09-13 09:09:45 -0300

    fravega prueba

--- scarp.py
+++ scarp.py
@@ -13,8 +13,10 @@ def scrapear_precio_producto(url):
             if "musimundo" in url:
                productos = sopa.find_all('div', class_='mus-product-box')
             elif 'mercadolibre' in url:
-                productos = sopa.find_all('li', class_='ui-search-layout__item')
-
+               productos = sopa.find_all('li', class_='ui-search-layout__item')
+            elif 'fravega' in url:
+               productos = sopa.find_all('article', class_='sc-812c6cb5-2')  # Cambia la clase según el HTML de Frávega
+               
             resultados = []
             
             for producto in productos:
@@ -33,6 +35,16 @@ def scrapear_precio_producto(url):
                     precio_tag = producto.find('span', class_='andes-money-amount__fraction')
                     titulo = titulo_tag.text.strip() if titulo_tag else 'Título no encontrado'
                     precio = precio_tag.text.strip() if precio_tag else 'Precio no encontrado'
+                                         
+                elif 'fravega' in url:
+                         # Buscar el título
+                    titulo_tag = producto.find('span', class_='sc-ca346929-0')  # Clase que contiene el título
+                    titulo = titulo_tag.text.strip() if titulo_tag else 'Título no encontrado'
+
+                    # Buscar el precio
+                    precio_tag = producto.find('span', class_='sc-1d9b1d9e-0')  # Clase que contiene el precio
+                    precio = precio_tag.text.strip() if precio_tag else 'Precio no encontrado'
+
                 
                 # Guardar el resultado
                 if titulo and precio:
@@ -49,7 +61,7 @@ def scrapear_precio_producto(url):
 
 # Función para probar el scraping con diferentes páginas y productos
 def funcion_prueba(pagina):
-    message = "scrape enova n50, radio"  # Cambia esto según sea necesario
+    message = "scrape playstation"  # Cambia esto según sea necesario
     queries = message[len('scrape '):].split(',')
 
     results = []
@@ -59,6 +71,8 @@ def funcion_prueba(pagina):
             url_producto = f"https://www.musimundo.com/search/?text={query}"
         elif pagina == 'ml':
             url_producto = f"https://listado.mercadolibre.com.ar/{query}#D[A:{query}]"
+        elif pagina == 'fv':
+             url_producto = f"https://www.fravega.com/l/?keyword={query}"
         
         print(f"Scraping en URL: {url_producto}")
         result = scrapear_precio_producto(url_producto)
@@ -67,5 +81,5 @@ def funcion_prueba(pagina):
     final_result = "\n\n".join(results)
     print(final_result)
 
-if __name__ == '__main__':
-    funcion_prueba('ml')
+if _name_ == '_main_':
+    funcion_prueba('fv')
